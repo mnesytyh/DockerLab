a224885daf1a31826f6ad8d69b1401008d091d76
Mikhail Nesytykh <mnesytyh@mail.ru>
2024-08-07 19:28:18 +0300

    Внесены изменения в скрипт postgresql/entrypoint.sh. Добавлено изменение владельца на монтируемые тома.

--- postgresql/entrypoint.sh
+++ postgresql/entrypoint.sh
@@ -2,7 +2,7 @@
 
 function postgres_start()
 {
-    pg_ctlcluster 15 main start
+    sudo su postgres -c "pg_ctlcluster 15 main start"
     until pg_isready -h localhost -p 5432
     do
         echo "Waiting for PostgreSQL to start..."
@@ -10,18 +10,18 @@ function postgres_start()
     done
     echo "PostgreSQL has been initialized."    
 }
-
+chown 1111:1111 /var/lib/postgresql /var/log/postgresql /etc/postgresql
 if test -z "$(ls -A /var/lib/postgresql)"; then
-    pg_createcluster 15 main 
+    sudo su postgres -c "pg_createcluster 15 main"
     postgres_start
-    psql -c "CREATE DATABASE wiki;"
-    psql -c "CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD';            
+    sudo su postgres -c "psql -c \"CREATE DATABASE wiki;\""
+    sudo su postgres -c "psql -c \"CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD';            
             CREATE USER wikijs WITH PASSWORD 'wikijsrocks';
             GRANT ALL PRIVILEGES ON DATABASE wiki TO wikijs;
-            ALTER DATABASE wiki OWNER TO wikijs;"
+            ALTER DATABASE wiki OWNER TO wikijs;\""
 else
     postgres_start
 fi
-tail -f /dev/null
+sudo su postgres -c "tail -f /dev/null"
 
 
